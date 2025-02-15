6fd62d02fa11697249f81989d22fa548638bf2a8
j3erdman <j3erdman@gmail.com>
2025-02-15 12:54:35 -0800

    Updated comments

--- data_collection/screen_data_collection.py
+++ data_collection/screen_data_collection.py
@@ -3,11 +3,13 @@ from PIL import Image, ImageGrab
 import pygetwindow as gw
 
 # Screenshotting Entire Screen
+# Returns a screenshot of the full screen
 fullScreenshot = ImageGrab.grab(bbox = None)
 
-# Get a list of windows with their basic information
+# Get a list of all windows with their basic information
+# Returns a list of all windows with their metadata
 allWindows = gw.getAllWindows()
 
-# Prints all windows' information
-for window in allWindows:
-    print(window)
+# Identify active window
+# Returns the active window's metadata
+activeWindow = gw.getActiveWindow()
