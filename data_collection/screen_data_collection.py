f0a050d6ad4c50c783a4c61bedcd547a93185c2f
j3erdman <j3erdman@gmail.com>
2025-02-15 12:51:58 -0800

    Added getting window information of ALL windows currently running on system

--- data_collection/screen_data_collection.py
+++ data_collection/screen_data_collection.py
@@ -1,4 +1,13 @@
-# Pillow Package
+# Packages
 from PIL import Image, ImageGrab
+import pygetwindow as gw
 
-screenshot = ImageGrab.grab(bbox = None)
+# Screenshotting Entire Screen
+fullScreenshot = ImageGrab.grab(bbox = None)
+
+# Get a list of windows with their basic information
+allWindows = gw.getAllWindows()
+
+# Prints all windows' information
+for window in allWindows:
+    print(window)
