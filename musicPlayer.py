50dbda1ac4d1961e3e2577eebc2a286c5dd9e9b5
Elliott-Hendrickson <100046451+Elliott-Hendrickson@users.noreply.github.com>
2025-02-15 16:55:02 -0800

    Kill me

--- musicPlayer.py
+++ musicPlayer.py
@@ -1,99 +1,101 @@
-
-# # Authenticate
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="5c346f6ca2124df9ab63bc476a9af418",
-#                                                client_secret="ea287964c0cf415ea45bff09135af98c",
-#                                                redirect_uri="http://localhost"))
-
-# # SPOTIPY_CLIENT_ID = "5c346f6ca2124df9ab63bc476a9af418"
-# # CLIENT_SECRET = "ea287964c0cf415ea45bff09135af98c"
-# # REDIRECT_URI = "http://localhost:8080"
-
-import os
-import random
 import requests
-import string
-import urllib
-from spotipy.oauth2 import SpotifyOAuth
+import spotipy
+import time
+from spotipy.oauth2 import SpotifyClientCredentials
 
-# Spotify API credentials
-CLIENT_ID = "your_client_id"
-CLIENT_SECRET = "your_client_secret"
-REDIRECT_URI = "http://localhost:8080"
+class SpotifyHandler():
 
-# Define the scope (modify as needed)
-SCOPE = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
+    def __init__(self, clientId, clientSecret):
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.authenticationToken = None
+        self.getAuthenticationToken()
+        self.sp = spotipy.Spotify(auth=self.authenticationToken)
+    """
+        Refreshes The Authentication Token
+    """
+    def getAuthenticationToken(self):
 
-requests.post()
+        TOKEN_URL = "https://accounts.spotify.com/api/token"
 
-# # Authenticate and get token
-# sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
-#                         client_secret=CLIENT_SECRET,
-#                         redirect_uri=REDIRECT_URI,
-#                         scope=SCOPE)
+        payload = {
+            "grant_type": "client_credentials",
+            "client_id": self.clientId,
+            "client_secret": self.clientSecret
+        }
 
-# token_info = sp_oauth.get_access_token(as_dict=True)
-# access_token = token_info['access_token']
-# print(access_token)
+        # Headers
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        response = requests.post(TOKEN_URL, data=payload, headers=headers)
+
+        if response.status_code == 200:
+            access_token = response.json().get("access_token")
+
+        self.authenticationToken=access_token
+        self.tokenDie = time.time() + response.json().get("expires_in")
+
+    """
+        Refreshes The Authentication Token if beyond expire time
+    """
+    def refreshAuthenticationToken(self):
+        
+        if self.tokenDie >= time.time():
+            self.getAuthenticationToken()
+
+    """
+        Gets The Recommended Song from genre
+    """
+    def getRecommendedSongSeed(self, genreList):
+
+        self.refreshAuthenticationToken()
+
+        print(self.authenticationToken)
+
+        # print(self.sp.me())
+
+        # print(self.sp.recommendations(seed_genres=genreList, limit=10))
+
+        # # Parse and print results
+        # if response.status_code == 200:
+
+        #     recommendations = response.json()
+        #     for i, track in enumerate(recommendations["tracks"], 1):
+        #         print(f"{i}. {track['name']} by {', '.join(artist['name'] for artist in track['artists'])}")
+
+        # else:
+        #     print("Error:", response.json())
+
+CLIENT_ID = "406941e40d6a47be81bc631cef061c37"
+CLIENT_SECRET = "bb36fc6a8b404925a4e3379a5cc6be70"
+
+# REDIRECT_URI = "http://localhost:8080"
+
+# # Set the required scopes for user authentication
+# scope = "user-read-private user-read-email"
+
+# from spotipy.oauth2 import SpotifyOAuth
+
+# # Use SpotifyOAuth for user authentication
+# auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope)
+
+# # Create the Spotify client with user authentication
+# spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+# # Fetch user information
+# print(spotify.me())
+
+# handler.getRecommendedSongSeed(["Pop"])
+
+# import spotipy
+# import spotipy.util as util
+# import random
 
 
-# class SpotifyClient(object):
-# 	def __init__(self, api_token):
-# 		self.api_token = api_token
+# token = spotipy.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET).get_access_token()
 
-# 	def get_random_tracks(self):
-# 		wildcard = f'%{random.choice(string.ascii_lowercase)}%'
-# 		query = urllib.parse.quote(wildcard)
-# 		offset = random.randint(0, 2000)
-# 		url = f"https://api.spotify.com/v1/search?q={query}&offset={offset}&type=track"
-# 		response = requests.get(
-# 			url,
-# 			headers={
-# 				"Content-Type": "application/json",
-# 				"Authorization": f"Bearer {self.api_token}"
-# 			}
-# 		)
-# 		response_json = response.json()
+# token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
-# 		# tracks = [
-# 		# 	track for track in response_json['tracks']['items']
-# 		# ]
-# 		print(response_json)
-
-# 		# print(f'Found {len(tracks)} tracks to add to your library')
-
-# 		# return tracks
-
-
-# 	def add_tracks_to_library(self, track_ids):
-# 		url = "https://api.spotify.com/v1/me/tracks"
-# 		response = requests.put(
-# 			url,
-# 			json={
-# 				"ids": track_ids
-# 			},
-# 			headers={
-# 				"Content-Type": "application/json",
-# 				"Authorization": f"Bearer {self.api_token}"
-# 			}
-# 		)
-		
-# 		return response.ok
-
-# def run():
-	
-#     token = os.getenv('SPOTIFY_AUTH_TOKEN')
-#     print(token)
-	
-#     spotify_client = SpotifyClient(os.getenv('SPOTIFY_AUTH_TOKEN'))
-#     random_tracks = spotify_client.get_random_tracks()
-# 	# track_ids = [track['id'] for track in random_tracks]
-
-# 	# was_added_to_library = spotify_client.add_tracks_to_library(track_ids)
-# 	# if was_added_to_library:
-# 	# 	for track in random_tracks:
-# 	# 		print(f"Added {track['name']} to your library")
-
-
-# if __name__ == '__main__':
-# 	run()
-
+# cache_token = token.get_access_token()
